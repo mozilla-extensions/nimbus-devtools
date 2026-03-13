mozilla-extensions/nimbus-devtools

commit 0944b8def793d272413a2092bfde785d2d1b079d

Add Experimenter message preview integration
This does not yet support translated messages or the multi-message
template.

Fixes #113

bin/build-manifest.mjs
@@ -19,8 +19,8 @@ const MANIFEST_PATH = path.join(SRC_DIR, MANIFEST);
 /**
  * Parse manifest.json and return the list of files required by the extension.
  */
-function parseManifest({ absolute = false } = {}) {
-  const files = [MANIFEST];
+function parseManifest({ absolute = false, dev = false } = {}) {
+  let files = [];
 
   const contents = fs.readFileSync(MANIFEST_PATH, {
     encoding: "utf-8",
@@ -28,6 +28,9 @@ function parseManifest({ absolute = false } = {}) {
   const manifest = JSON.parse(contents);
 
   files.push(...(manifest.background?.scripts ?? []));
+  files.push(
+    ...(manifest.content_scripts?.flatMap((script) => script.js) ?? []),
+  );
 
   for (const api of Object.values(manifest.experiment_apis ?? {})) {
     files.push(api.schema);
@@ -37,17 +40,23 @@ function parseManifest({ absolute = false } = {}) {
     }
 
     if (api.child) {
-      files.push(api.parent.child);
+      files.push(api.child.script);
     }
   }
 
   files.push(...Object.values(manifest.icons ?? {}));
 
   if (absolute) {
-    return files.map((f) => path.join(SRC_DIR, f));
+    files = files.map((f) => path.join(SRC_DIR, f));
   }
 
-  return files;
+  if (dev) {
+    for (const contentScript of manifest.content_scripts) {
+      contentScript.matches.push("http://localhost/*");
+    }
+  }
+
+  return { manifest, files };
 }
 
 function copyFiles(files) {
@@ -68,7 +77,7 @@ function watch() {
   let watcher;
 
   function makeWatcher() {
-    const files = parseManifest({ absolute: true });
+    let { manifest, files } = parseManifest({ absolute: true, dev: true });
 
     watcher = new Watcher(files);
     watcher.on("all", (event, targetPath) => {
@@ -77,7 +86,13 @@ function watch() {
         makeWatcher();
       }
 
-      copyFiles(parseManifest());
+      ({ manifest, files } = parseManifest({ dev: true }));
+
+      fs.writeFileSync(
+        path.join(DIST_DIR, MANIFEST),
+        JSON.stringify(manifest, null, 2),
+        { encoding: "utf-8" },
+      );
     });
   }
 
@@ -87,5 +102,12 @@ function watch() {
 if (process.argv.length === 3 && process.argv[2] == "--watch") {
   watch();
 } else {
-  copyFiles(parseManifest());
+  const { manifest, files } = parseManifest();
+
+  copyFiles(files);
+  fs.writeFileSync(
+    path.join(DIST_DIR, MANIFEST),
+    JSON.stringify(manifest, null, 2),
+    { encoding: "utf-8" },
+  );
 }

eslint.config.mjs
@@ -57,7 +57,7 @@ export default defineConfig([
   },
   {
     name: "nimbus-devtools/webext/background",
-    files: ["./src/background.js"],
+    files: ["./src/background.js", "./src/experimenter.js"],
     languageOptions: {
       ecmaVersion: 2023,
       globals: {
@@ -68,7 +68,7 @@ export default defineConfig([
   },
   {
     name: "nimbus-devtools/webext/api",
-    files: ["./src/apis/**.js"],
+    files: ["./src/apis/**/*.js"],
     extends: [
       mozilla.configs["flat/recommended"],
       // The mozilla/recommended/system-modules configuration only applies to
@@ -88,12 +88,28 @@ export default defineConfig([
     languageOptions: {
       globals: {
         ...globals.browser,
+        Cu: true,
         ExtensionAPI: true,
         ExtensionUtils: true,
       },
     },
     rules: {
+      // WebIDL interfaces are not exposed to WebExtension APIs by default.
+      "mozilla/reject-importGlobalProperties": "off",
+    },
+  },
+  {
+    name: "nimbus-devtools/webext/api/nimbus",
+    files: ["./src/apis/nimbus.js"],
+    rules: {
       "no-unused-vars": ["error", { varsIgnorePattern: "nimbus" }],
+    },
+  },
+  {
+    name: "nimbus-devtools/webext/api/messagingSystem",
+    files: ["./src/apis/messagingSystem.js"],
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "messagingSystem" }],
     },
   },
   {

src/apis/messagingSystem.js
@@ -1,0 +1,71 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+Cu.importGlobalProperties(["URL"]);
+
+const lazy = {};
+
+ChromeUtils.defineESModuleGetters(lazy, {
+  MESSAGING_EXPERIMENTS_DEFAULT_FEATURES:
+    "resource:///modules/asrouter/MessagingExperimentConstants.sys.mjs",
+
+  AboutMessagePreviewParent:
+    "resource:///actors/AboutMessagePreviewParent.sys.mjs",
+});
+
+var messagingSystem = class extends ExtensionAPI {
+  getAPI() {
+    function getMessagePreviewURL(rawMessage) {
+      const codeUnits = new Uint16Array(rawMessage.length);
+      for (let i = 0; i < codeUnits.length; i++) {
+        codeUnits[i] = rawMessage.charCodeAt(i);
+      }
+
+      const url = new URL("about:messagepreview");
+      url.searchParams.append(
+        "json",
+        new Uint8Array(codeUnits.buffer).toBase64(),
+      );
+      return url.toString();
+    }
+
+    function openTabPrivileged(url) {
+      const { gBrowser } = Services.wm.getMostRecentWindow("navigator:browser");
+      gBrowser.selectedTab = gBrowser.addTab(url, {
+        triggeringPrincipal:
+          Services.scriptSecurityManager.getSystemPrincipal(),
+      });
+    }
+
+    return {
+      experiments: {
+        messagingSystem: {
+          getMessagingFeaturesAndTemplates() {
+            let templates;
+            try {
+              templates =
+                lazy.AboutMessagePreviewParent.getSupportedTemplates();
+            } catch {
+              return null;
+            }
+
+            return {
+              featureIds: lazy.MESSAGING_EXPERIMENTS_DEFAULT_FEATURES,
+              templates,
+            };
+          },
+
+          previewMessage(content) {
+            Services.prefs.setBoolPref(
+              "browser.newtabpage.activity-stream.asrouter.devtoolsEnabled",
+              true,
+            );
+            openTabPrivileged(getMessagePreviewURL(content));
+          },
+        },
+      },
+    };
+  }
+};

src/background.js
@@ -8,3 +8,39 @@ browser.browserAction.onClicked.addListener(() =>
     url: "./ui/index.html",
   }),
 );
+
+browser.runtime.onMessage.addListener(
+  function (message, _sender, sendResponse) {
+    console.log(message);
+    if (
+      typeof message !== "object" ||
+      message === null ||
+      typeof message.kind !== "string"
+    ) {
+      console.error("nimbus-devtools: unexpected message", message);
+      return;
+    }
+
+    switch (message.kind) {
+      case "nimbus-devtools:getMessagingFeaturesAndTemplates":
+        browser.experiments.messagingSystem
+          .getMessagingFeaturesAndTemplates()
+          .then((v) => {
+            console.log("rv", v);
+            sendResponse(v);
+          });
+
+        return true;
+
+      case "nimbus-devtools:previewMessage":
+        browser.experiments.messagingSystem.previewMessage(message.message);
+        break;
+
+      default:
+        console.error("nimbus-devtools: unexpected message", message);
+        break;
+    }
+
+    return undefined;
+  },
+);

src/experimenter.js
@@ -1,0 +1,215 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+(async function () {
+  const match = new URLPattern({ pathname: "/nimbus/:slug/:view/" }).exec(
+    document.location,
+  );
+  if (!match) {
+    return;
+  }
+
+  const { view } = match.pathname.groups;
+  if (!["summary", "update_branches"].includes(view)) {
+    return;
+  }
+
+  const experimentMetadata = getExperimentMetadata();
+  if (
+    !experimentMetadata ||
+    experimentMetadata?.application !== "firefox-desktop"
+  ) {
+    return;
+  }
+
+  const messaging = await getMessagingFeaturesAndTemplates();
+  if (!messaging) {
+    return;
+  }
+
+  const integration = new ExperimenterIntegration(
+    experimentMetadata,
+    messaging,
+  );
+
+  switch (view) {
+    case "summary":
+      return integration.handleSummaryPage();
+
+    case "update_branches":
+      return integration.handleUpdateBranchesPage();
+  }
+})();
+
+function getExperimentMetadata() {
+  const $metadata = document.getElementById(
+    "nimbus-devtools-experiment-metadata",
+  );
+  if (!$metadata) {
+    return;
+  }
+
+  try {
+    return JSON.parse($metadata.textContent);
+  } catch {
+    return null;
+  }
+}
+
+async function getMessagingFeaturesAndTemplates() {
+  return browser.runtime.sendMessage({
+    kind: "nimbus-devtools:getMessagingFeaturesAndTemplates",
+  });
+}
+
+class ExperimenterIntegration {
+  constructor(
+    experimentMetadata,
+    { templates: supportedTemplates, featureIds },
+  ) {
+    this.experimentMetadata = experimentMetadata;
+    this.supportedTemplates = supportedTemplates;
+    this.featureIds = featureIds;
+  }
+
+  /**
+   * Update the Experimenter summary page.
+   */
+  handleSummaryPage() {
+    document
+      .querySelectorAll("textarea[data-nimbus-devtools-feature-value]")
+      .forEach((el) => {
+        if (!this.featureIds.includes(el.dataset.featureId)) {
+          return;
+        }
+
+        const container = el.closest(".readonly-json-collapsible");
+        if (!container) {
+          return;
+        }
+
+        const result = this.validateMessage(el.value);
+        if (result.error) {
+          container
+            .querySelectorAll("[data-nimbus-devtools-cannot-preview-notice]")
+            .forEach((notice) => {
+              notice.title = `Cannot preview message: ${result.error}`;
+              notice.classList.remove("d-none");
+            });
+        } else {
+          container
+            .querySelectorAll("[data-nimbus-devtools-preview-button]")
+            .forEach((btn) => {
+              btn.addEventListener("click", () =>
+                this.previewMessage(el.value),
+              );
+              btn.classList.remove("d-none");
+            });
+        }
+      });
+  }
+
+  /**
+   * Update the Experimenter update branches page.
+   */
+  handleUpdateBranchesPage() {
+    const insertControls = () => {
+      document
+        .querySelectorAll("[data-nimbus-devtools-feature-editor]")
+        .forEach((editor) => {
+          if (
+            !Object.hasOwn(editor.dataset, "featureId") ||
+            !this.featureIds.includes(editor.dataset.featureId)
+          ) {
+            return;
+          }
+
+          const input = editor.querySelector("input.value-editor");
+          const btn = editor.querySelector(
+            "[data-nimbus-devtools-try-preview-button]",
+          );
+          btn.addEventListener("click", () =>
+            this.tryPreviewMessage(input.value),
+          );
+          btn.classList.remove("d-none");
+        });
+    };
+
+    document.body.addEventListener("htmx:afterSwap", (event) => {
+      if (
+        ["branches-form", "content-with-sidebar"].includes(
+          event.detail.target.id,
+        )
+      ) {
+        insertControls();
+      }
+    });
+
+    insertControls();
+  }
+
+  async tryPreviewMessage(message) {
+    const result = this.validateMessage(message);
+    if (result.error) {
+      this.createAndShowToast(`Could not preview message: ${result.error}`, {
+        classList: ["text-bg-danger"],
+      });
+
+      return;
+    }
+
+    return this.previewMessage(message);
+  }
+
+  async previewMessage(message) {
+    return browser.runtime.sendMessage({
+      kind: "nimbus-devtools:previewMessage",
+      message,
+    });
+  }
+
+  createAndShowToast(content, { classList = [] } = {}) {
+    const el = document
+      .importNode(document.getElementById("template-toast").content, true)
+      .querySelector(".toast");
+    console.log(el);
+
+    el.querySelector(".toast-body").appendChild(new Text(content));
+
+    if (classList) {
+      for (const className of classList) {
+        el.classList.add(className);
+      }
+    }
+
+    el.addEventListener("hidden.bs.toast", () => document.body.removeChild(el));
+    document.getElementById("toasts-container").appendChild(el);
+
+    new window.wrappedJSObject.bootstrap.Toast(el).show();
+  }
+
+  validateMessage(message) {
+    let messageJson;
+    try {
+      messageJson = JSON.parse(message);
+    } catch {
+      return { error: "JSON parse error" };
+    }
+
+    if (typeof messageJson !== "object" || messageJson === null) {
+      return { error: "Message is not a JSON object" };
+    }
+
+    if (typeof messageJson.template === "undefined") {
+      return { error: "Message template is undefiend" };
+    }
+
+    if (!this.supportedTemplates.includes(messageJson.template)) {
+      return { error: "Unsupported template" };
+    }
+
+    return { ok: true };
+  }
+}

src/types/global.d.ts
@@ -105,6 +105,17 @@ declare module "mozjexl/lib/parser/Parser" {
   }
 }
 
+type MessagingFeaturesAndTemplates = {
+  featureIds: string[];
+  templates: string[];
+};
+
+declare namespace browser.experiments.messagingSystem {
+  function getMessagingFeaturesAndTemplates(): Promise<null | MessagingFeaturesAndTemplates>;
+
+  function previewMessage(string): Promise<void>;
+}
+
 type EnrollInExperimentResult =
   | {
       enrolled: true;

commit 4db3d3b92bb880a7e3334fdd77b1535ba775d9f2

Fix watch:manifest script
Commit 0944b8def793d272413a2092bfde785d2d1b079d broke the script while
refactoring `parseManifest()` to return the manifest for modification.

bin/build-manifest.mjs
@@ -88,6 +88,7 @@ function watch() {
 
       ({ manifest, files } = parseManifest({ dev: true }));
 
+      copyFiles(files);
       fs.writeFileSync(
         path.join(DIST_DIR, MANIFEST),
         JSON.stringify(manifest, null, 2),